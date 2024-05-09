2ddbf8b15c3ff7b9b2afb49f0316b57d8510629e
Wai Lynn Zaw <wailynnzaw.lamz@gmail.com>
2024-05-09 17:38:18 +0700

    update / delete for artists

--- app.py
+++ app.py
@@ -359,27 +359,43 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
+  artist = Artist.query.filter(Artist.id == artist_id).one_or_none().format()
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
-
+  artist = Artist.query.filter(Artist.id == artist_id).one_or_none()
+  error = False
+  try:  
+        artist.name = request.form['name']
+        artist.city = request.form['city']
+        artist.state = request.form['state']
+        artist.phone = request.form['phone']
+        generes_array = request.form.getlist('genres')
+        artist.genres = ','.join(generes_array)
+        artist.website = request.form['website_link']
+        artist.image_link = request.form['image_link']
+        artist.facebook_link = request.form['facebook_link']
+        if request.form['seeking_venue'] is 'y':
+          artist.seeking_venue = True
+        else:
+          artist.seeking_venue = False
+        artist.seeking_description = request.form['seeking_description']
+        db.session.add(artist)
+        db.session.commit()
+  except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+  finally:
+        db.session.close()
+        if error:
+            flash('An error occurred. Artist ' +
+                  request.form['name'] + ' could not be updated.')
+        else:
+            flash('Artist ' + request.form['name'] +
+                  ' was successfully updated!')
+        
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/artists/create', methods=['GET'])
